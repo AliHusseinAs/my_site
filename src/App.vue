05e7d80fe9585c08fd16c3a35a2c86bcf577f5e2
Ali Hussein <alihussein10x@gmail.com>
2025-06-07 00:32:28 +0300

    website

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import background from '@/components/background.vue'
+import navBar from '@/components/nav.vue'
+import slogan from '@/components/slogan.vue'
+import skills from '@/components/skills.vue'
+import cv from '@/components/cv.vue'
+import projects from '@/components/projects.vue'
+</script>
+
+
+<template>
+  
+  <background>
+    <navBar/>
+    <slogan/>
+    <projects/>
+    <skills/>
+    <cv/>
+  </background>
+  
+</template>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'devicon/devicon.min.css'
+
+createApp(App).mount('#app')
